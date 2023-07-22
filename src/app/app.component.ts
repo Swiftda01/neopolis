fef7197b34d120f720217ab53c61b646b727a836
Dave <david.anthony.swift@gmail.com>
2023-07-22 13:36:31 +0200

    Fade in landscape on page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { ContractService } from './services/contract.service';
 import web3Utils from './utils/web3';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(2000, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   connectedToNeonNetwork = false;
